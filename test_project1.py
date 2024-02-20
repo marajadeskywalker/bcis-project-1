fcdde4e325834393475f0d81de45ea88dc7c8a5f
marajadeskywalker <89876990+marajadeskywalker@users.noreply.github.com>
2024-02-20 17:16:14 -0500

    finished docstrings and comments

--- test_project1.py
+++ test_project1.py
@@ -1,8 +1,16 @@
+"""
+test_project1.py
+Calls the functions written in project1.py for testing purposes
+Written by Lexi Reinsborough and Ashley Heath
+"""
+#import packages
 from matplotlib import pyplot as plt
 import numpy as np
 import project1
 
+#run functions
 target_erp, nontarget_erp, target_std, nontarget_std, erp_times, target_epochs, nontarget_epochs = project1.erp_by_subject(3)
+project1.plot_erp_intervals(target_erp, nontarget_erp, target_std, nontarget_std, erp_times, subject=3)
 p_values = project1.bootstrap(target_erp, nontarget_erp, target_epochs, nontarget_epochs)
-project1.plot_fdr_corrected_ps(target_erp, nontarget_erp, target_std, nontarget_std, erp_times, p_values, subject=3)
+rejection_array = project1.plot_fdr_corrected_ps(target_erp, nontarget_erp, target_std, nontarget_std, erp_times, p_values, subject=3)
 project1.evaluate_across_subjects()
